3a02a8446a4e46e0fb2f87a2016cc75e25c1e6b6
Silviu Rada <silviu.rada@gmail.com>
2020-09-18 13:22:17 +0300

    change port 8081

--- src/app/restapiservice/rest-api-service.service.ts
+++ src/app/restapiservice/rest-api-service.service.ts
@@ -9,45 +9,45 @@ export class RestApiServiceService {
   constructor(private http: HttpClient) { }
 
   getAllUsers() {
-    return this.http.get("http://localhost:7070/user");
+    return this.http.get("http://localhost:8081/api/user");
   }
 
   getAllClients() {
-    return this.http.get("http://localhost:7070/client");
+    return this.http.get("http://localhost:8081/api/client");
   }
   deleteClient(id){
-    return this.http.request('delete', "http://localhost:7070/client/" + id);
+    return this.http.request('delete', "http://localhost:8081/api/client/" + id);
   }
 
   getAllRoles() {
-    return this.http.get("http://localhost:7070/role");
+    return this.http.get("http://localhost:8081/api/role");
   }
 
   addRole(role) {
-    return this.http.post<any>("http://localhost:7070/role", { name: role });
+    return this.http.post<any>("http://localhost:8081/api/role", { name: role });
   }
 
   deleteRole(id) {
-    return this.http.request('delete', "http://localhost:7070/role/" + id);
+    return this.http.request('delete', "http://localhost:8081/api/role/" + id);
   }
 
   addClient(clientName) {
-    return this.http.post<any>("http://localhost:7070/client", { name: clientName });
+    return this.http.post<any>("http://localhost:8081/api/client", { name: clientName });
   }
 
   addUser(username) {
-    return this.http.post<any>("http://localhost:7070/user", { username: username });
+    return this.http.post<any>("http://localhost:8081/api/user", { username: username });
   }
 
   addUserRole(newRole, id) {
-    return this.http.post<any>("http://localhost:7070/user/" + id + "/addRole", { name: newRole });
+    return this.http.post<any>("http://localhost:8081/api/user/" + id + "/addRole", { name: newRole });
   }
 
   deleteUserRole(role, id) {
-    return this.http.request('delete', "http://localhost:7070/user/" + id + "/removeRole", { body: { name: role }});
+    return this.http.request('delete', "http://localhost:8081/api/user/" + id + "/removeRole", { body: { name: role }});
   }
 
   getUserByUsername(username) {
-    return this.http.get("http://localhost:7070/user/" + username)
+    return this.http.get("http://localhost:8081/api/user/" + username)
   }
 }
