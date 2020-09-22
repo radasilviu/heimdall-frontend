bf63fef7d52d59c1fc89fe6c76514358b4da2be6
Silviu Rada <silviu.rada@gmail.com>
2020-09-22 13:21:51 +0300

    add some validation, adapt template for client login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { error } from 'protractor';
-import { AdminAuthService} from '../../services/admin-auth/admin-auth.service';
+import { AdminAuthService } from '../../services/admin-auth/admin-auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +10,26 @@ import { AdminAuthService} from '../../services/admin-auth/admin-auth.service';
 })
 
 export class LoginComponent implements OnInit {
-  username;
-  password;
+
   errormessaje= false;
   constructor(private authService: AdminAuthService, private router: Router) { }
 
-  ngOnInit(): void {}
+  loginForm: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [ Validators.required]),
+      password: new FormControl('', [ Validators.required])
+    });
+  }
 
   login(): void {
+
+    const username = this.loginForm.get('username').value;
+    const password = this.loginForm.get('password').value;
+
     this.authService
-      .login(this.username, this.password)
+      .login(username,password)
       .subscribe(
         user => {
           this.router.navigate(['home']);
