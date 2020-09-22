22e27bff7a304f7da61c1cdfed13f46739e505fc
Vinter Antonio <tonyivinter@gmail.com>
2020-09-22 15:02:34 +0300

    added dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule , routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations'
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import {MatListModule} from '@angular/material/list';
 import {MatInputModule} from '@angular/material/input';
@@ -21,7 +21,12 @@ import { HeimdallRolesComponent } from './components/heimdall-roles/heimdall-rol
 import { ClientLoginComponent } from './components/client-login/client-login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import {AuthInterceptor} from './interceptors/auth/auth.interceptor';
-import {MatSnackBarModule} from '@angular/material/snack-bar'; 
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
+
+
+
 
 @NgModule({
   declarations: [
@@ -49,7 +54,16 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     MatInputModule,
     NoopAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
