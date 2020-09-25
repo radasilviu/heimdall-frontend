fc1bdc7f0f83b28ca14abef3ee3dd83b9a834e05
gabrielserban <sgaby100@gmail.com>
2020-09-25 14:19:56 +0300

    implemented form verification for registration page

--- src/app/components/registration-page/registration-page.component.ts
+++ src/app/components/registration-page/registration-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-registration-page',
@@ -9,17 +10,25 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class RegistrationPageComponent implements OnInit {
   errormessaje:string;
   loginForm: FormGroup;
-  
+  passwordMatch:boolean = true;
   constructor() { }
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
       Username: new FormControl('', [ Validators.required]),
       Password: new FormControl('', [ Validators.required]),
+      ConfirmPassword: new FormControl('', [ Validators.required]),
       Email: new FormControl('', [ Validators.required]),
     });
   }
 
   register(){
+    var user = new User();
+    if(this.loginForm.get('Password').value == this.loginForm.get('ConfirmPassword').value){
+      this.passwordMatch =  true;
+    }
+    else{
+      this.passwordMatch =  false;
+    }
   }
 }
