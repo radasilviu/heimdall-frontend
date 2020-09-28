09acaae22c1000544a7e15f6e74b20c395be2527
gabrielserban <sgaby100@gmail.com>
2020-09-28 15:44:38 +0300

    added email field for registration

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,25 +1,26 @@
 import { IRole } from './Role';
-
-export interface IUser {
-  username?:string
-  password?:string
-  access_token?: string;
-  roles?:IRole[]
-}
-export class User implements IUser{
-  public constructor(
-    public username?: string,
-    public password?:string,
-    public access_token?:string,
-    public roles?:IRole[]
-  ){}
  
-  setUsername(username){
-    this.username = username;
-  }
-  setUsernameAndPassword(username,password){
-    this.username = username;
-    this.password = password;
-
-  }
+export interface IUser {
+ username?: string;
+ password?: string;
+ access_token?: string;
+ roles?: IRole[];
+}
+export class User implements IUser {
+ public constructor(
+ public username?: string,
+ public password?: string,
+ public email?: string,
+ public access_token?: string,
+ public roles?: IRole[],
+ public isActivated?: Boolean
+ ) {}
+ 
+ setUsername(username) {
+ this.username = username;
+ }
+ setUsernameAndPassword(username, password) {
+ this.username = username;
+ this.password = password;
+ }
 }
