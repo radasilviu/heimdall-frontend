414532366165f61f43d5ae7132cd047e40876691
gabrielserban <sgaby100@gmail.com>
2020-09-30 12:41:28 +0300

    added delete-dialog

--- src/app/components/dialogs/delete-dialog/delete-dialog.component.ts
+++ src/app/components/dialogs/delete-dialog/delete-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { RestApiServiceService } from 'src/app/services/restapiservice/rest-api-service.service';
 
 @Component({
   selector: 'app-delete-dialog',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private service: RestApiServiceService,
+    public dialogRef: MatDialogRef<DeleteDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
   }
-
+  
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  
 }
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -4,8 +4,9 @@ import { Router } from '@angular/router';
 import { RestApiServiceService } from '../../services/restapiservice/rest-api-service.service';
 import { RoleServiceService } from '../../services/roleservice/role-service.service';
 import { UserDialogComponent } from '../dialogs/user-dialog/user-dialog.component';
-import {User} from '../../models/User';
+import { User } from '../../models/User';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DeleteDialogComponent } from '../dialogs/delete-dialog/delete-dialog.component';
 
 
 @Component({
@@ -15,13 +16,13 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class UsersComponent implements OnInit {
   displayedColumns = ['username', 'role'];
-  allUsers:User[];
-  user:User;
-  isLoading:boolean = false;
+  allUsers: User[];
+  user: User;
+  isLoading: boolean = false;
   form = new FormGroup({
-    username:new FormControl('', Validators.required),
-    password:new FormControl('',Validators.required),
-    email:new FormControl('',Validators.email)
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.email)
 
   })
 
@@ -36,14 +37,15 @@ export class UsersComponent implements OnInit {
     this.getAllUsers();
   }
 
-  onSubmit(){
-    this.addUser(this.form.value.username,this.form.value.password,this.form.value.email)
+  onSubmit() {
+    this.addUser(this.form.value.username, this.form.value.password, this.form.value.email)
   }
 
-  openDialog(currentUserName:string) {
+
+  updateUserDialog(currentUserName: string) {
     const dialogRef = this.dialog.open(UserDialogComponent);
     dialogRef.afterClosed().subscribe(data => {
-     let  user = new User(data)
+      let user = new User(data)
       if (data !== undefined) {
         this.service.updateUserName(currentUserName, user).subscribe(data => {
           this.getAllUsers();
@@ -53,8 +55,22 @@ export class UsersComponent implements OnInit {
       }
     });
   }
-  updateUser(currentUserName:string) {
-    this.openDialog(currentUserName)
+  deleteUserDialog(username:string) {
+    const dialogRef = this.dialog.open(DeleteDialogComponent);
+    dialogRef.afterClosed().subscribe(data => {
+      if (data == "true") {
+        this.service.deleteUser(username).subscribe(data => {
+          this.getAllUsers();
+        });
+      }
+      else {
+        console.log("nu")
+      }
+    })
+  }
+
+  updateUser(currentUserName: string) {
+    this.updateUserDialog(currentUserName)
   }
 
   getAllUsers() {
@@ -64,26 +80,25 @@ export class UsersComponent implements OnInit {
     })
   }
 
-  addUser(username:string,password:string,email:string) {
-    let user = new User(username,password,email)
+  addUser(username: string, password: string, email: string) {
+    let user = new User(username, password, email)
     this.isLoading = true;
-    this.service.addUser(user).toPromise().then(data=>{
-      this.isLoading= false
+    this.service.addUser(user).toPromise().then(data => {
+      this.isLoading = false
     })
-    .catch((err) =>{
-      this.service.openSnackBar(err.error,1500);
-    })
+      .catch((err) => {
+        this.service.openSnackBar(err.error, 1500);
+      })
   }
 
-  
 
-  deleteUser(username:string) {
-    this.service.deleteUser(username).subscribe(data => {
-      this.getAllUsers();
-    });
+
+  deleteUser(username: string) {
+    this.deleteUserDialog(username);
+
   }
 
-  userRoles(user:string) {
+  userRoles(user: string) {
     this.roleService.setUserName(user)
     this.router.navigate(['home/users/roles']);
   }
